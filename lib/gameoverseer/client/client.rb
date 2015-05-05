5f45b8fc3fe1dd6f3bb8b8ee68d43c9b05ab15e0
Cyberarm <matthewlikesrobots@gmail.com>
2015-05-05 08:03:45 -0500

    Initial commit

---
+++ lib/gameoverseer/client/client.rb
@@ -0,0 +1,43 @@
+require "renet"
+require "multi_json"
+
+class GameOverseer
+  class Client
+    CHAT = 0 # channel int. used for in-game chat
+    WORLD= 1 # channel int. used for sending/receiving world state updates
+    HANDSHAKE = 2 # channel int. used for authentication or similar purposes
+    FAULT=3 # channel int. used to send/receive errors
+
+    def initialize(host = "localhost", port = 56789, compression = true)
+      @host = host
+      @port = port
+      @compression = compression
+      
+      @socket = ENet::Connection.new(host, port, 4, 0, 0)
+      @socket.use_compression(compression)
+      connect
+    end
+
+    def connect(timeout = 1000) # default to 1 second
+      # TODO: gracefully attempt to connect to Server.
+    end
+
+    def disconnect(timeout = 1000)
+      # TODO: Gracefully attempt to connect to Server.
+    end
+
+    def send(channel, mode, hash, channel_id = WORLD, reliable = false)
+      # channel is a String
+      # mode is a ?
+      # hash is a Hash
+      # reliable is a Boolean
+
+      message = MultiJson.dump({channel: channel, mode: mode, data: hash})
+      @socket.send_packet(message, reliable, channel_id)
+    end
+
+    def handle_packet(method)
+      # Do stuff
+    end
+  end
+end
